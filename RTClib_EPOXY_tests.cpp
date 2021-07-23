45954fdd718cb584558958073db4e769b1354d01
Caio Pellicani <caiopellicani@hotmail.com>
2021-07-23 19:11:56 -0300

    IMP: tests

---
+++ RTClib_EPOXY_tests.cpp
@@ -0,0 +1,13 @@
+#include <AUnit.h>
+#include "RTClib_EPOXY.h"
+
+using namespace aunit;
+
+test(RTClib_EPOXY_ds3231) {
+  RTC_DS3231 rtc;
+  rtc.adjust( DateTime( 2021, 6, 21, 6, 15, 50 ) );
+  
+  assertEqual(rtc.now().timestamp( rtc.now().TIMESTAMP_TIME ), "06:15:50" );
+  delay( 1000 );
+  assertEqual(rtc.now().timestamp( rtc.now().TIMESTAMP_TIME ), "06:15:51" );
+}
