4ebddeb9e3f51b35c7b9703e4ab96a3bf79514c0
Caio Pellicani <caiopellicani@hotmail.com>
2021-07-26 11:39:56 -0300

    BUGFIX: faltava rtc.begin()

--- RTClib_EPOXY_tests.cpp
+++ RTClib_EPOXY_tests.cpp
@@ -5,9 +5,11 @@ using namespace aunit;
 
 test(RTClib_EPOXY_ds3231) {
   RTC_DS3231 rtc;
-  rtc.adjust( DateTime( 2021, 6, 21, 6, 15, 50 ) );
-  
-  assertEqual(rtc.now().timestamp( rtc.now().TIMESTAMP_TIME ), "06:15:50" );
-  delay( 1000 );
-  assertEqual(rtc.now().timestamp( rtc.now().TIMESTAMP_TIME ), "06:15:51" );
+  if( rtc.begin() ){
+    rtc.adjust( DateTime( 2021, 6, 21, 6, 15, 50 ) );
+    
+    assertEqual(rtc.now().timestamp( rtc.now().TIMESTAMP_TIME ), "06:15:50" );
+    delay( 1000 );
+    assertEqual(rtc.now().timestamp( rtc.now().TIMESTAMP_TIME ), "06:15:51" );
+  }
 }
